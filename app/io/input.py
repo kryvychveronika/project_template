4c83741ad6f24bba8c234e32fe3335518df6c664
kryvychveronika <kryvych.veronika@gmail.com>
2025-04-30 04:14:09 +0300

    Added functions

---
+++ app/io/input.py
@@ -0,0 +1,8 @@
+def get_input():
+    pass
+
+def file_reading_python():
+    pass
+
+def file_reading_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,8 @@
+def print_out():
+    pass
+
+def file_writing_python():
+    pass
+
+def file_writing_pandas():
+    pass
